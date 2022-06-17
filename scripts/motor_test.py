8920a72db51e228e282f6d102fdf13b5359a5e5d
18C1054_S_K <s18c1054uc@s.chibakoudai.jp>
2022-06-17 11:07:20 +0900

    fix and add motor_test

--- scripts/motor_test.py
+++ scripts/motor_test.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
-import gpiozero
 import rospy
-from std_msgs.msg import Float32, Bool
+import gpiozero
 
 #rad
 class MyMotor():
-	DELTA_T = 0.003
+	DELTA_T = 0.002
 	target_angv = 0.0
 	STEP_2_RAD = 3.14159 / 100.0
 	angv = 0.0
@@ -28,6 +27,9 @@ class MyMotor():
 		self.encoder = gpiozero.RotaryEncoder(a=encoder_a_gpiono, b=encoder_b_gpiono, max_steps=100)
 		self.timer = rospy.Timer(rospy.Duration(self.DELTA_T), self.timer_func)
 
+		self.k_p = rospy.get_param('/inv__pendulum/k_p')
+		self.k_i = rospy.get_param('/inv__pendulum/k_i')
+		self.k_d = rospy.get_param('/inv__pendulum/k_d')
 		self.latest_angs = [0.0] * self.l
 		self.s = 1.0 - (0.5 ** self.l)
 
@@ -70,56 +72,14 @@ class MyMotor():
 		self.err_ang = 0.0
 
 
-class TireRotaterNode():
-	POS_V_2_RAD_PER_SEC = 0.0
-	MAX_RAD_PER_SEC = 5.0
-
-	rad_per_sec = 0.0
-	
-	controll_failed = False
-
-	def __init__(self):
-		self.motor = gpiozero.Motor(17, 18)
-		self.sub_pos_v('pos_v', Float32, self.on_update_pos_v)
-		self.sub_fail('failed', Bool self.on_controll_failed)
-
-
-	def on_update_pos_v(self, msg):
-		if not controll_failed:
-			self.rad_per_sec = msg.data * POS_V_2_RAD_PER_SE
-			o = self.rad_per_sec / self.MAX_RAD_PER_SEC
-			if o >= 1.0:
-				self.motor.forward(1.0)
-			elif o >=0.0:
-				self.motor.forward(o)
-			if o < -1.0:
-				self.motor.backward(1.0)
-			elif o < 0.0:
-				self.motor.backward(-o)
-
-
-	def on_controll_failed(self, msg):
-		if msg.data == True:
-			self.controll_failed = True
-			delta_t = 1.0 / 30.0
-			r = rospy.Rate(30)
-			while self.rad_per_sec > 0.001 or self.rad_per_sec < -0.001:
-				self.rad_per_sec = - self.rad_per_sec * dalta_t
-				if self.rad_per_sec < 0.0:
-					self.motor.backward(- self.rad_per_sec / self.MAX_RAD_PER_SEC)
-				else:
-					self.motor.forward(self.rad_per_sec / self.MAX_RAD_PER_SEC)
-				r.sleep()
-			self.motor.stop()
-
-
 def main():
 	try:
-		rospy.init_node('tire_rotater', anonymous=True)
-		node = TireControllerNode()
+		rospy.init_node('motor_test', anonymous=True)
+		mymotor = MyMotor(23, 24, 5, 6)
 		rospy.spin()
 	except rospy.ROSInterruptException: pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 	main()
+
