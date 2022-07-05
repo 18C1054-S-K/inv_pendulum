5c71842229edad5aa89c51a57c7a9b433a57fcb2
18C1054_S_K <s18c1054uc@s.chibakoudai.jp>
2022-07-05 14:33:27 +0900

    ang_talker 2 state_talker

---
+++ scripts/check.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs import Float32MultiArray
+
+
+class CheckNode:
+	pendulum_ang = 0.0
+	pendulum_angv = 0.0
+	car_pos = 0.0
+	car_vel = 0.0
+	
+	motorL_angv = 0.0
+	motorR_angv = 0.0
+
+
+	def __init__(self):
+		rospy.init_node('check', anonymous=True)
+		self.sub_state = rospy.Subscriber('state', Float32MultiArray, self.OnUpdateState)
+		self.sub_motor = rospy.Subscriber('motor', Float32MultiArray, self.OnUpdateMotor) #debug
+		self.timer_disp = rospy.Timer(rospy.Duration(1.2), self.Disp)
+
+
+	def OnUpdateState(self, msg):
+		self.pendulum_ang = msg.data[0]
+		self.pendulum_angv = msg.data[1]
+		self.car_pos = msg.data[2]
+		self.car_vel = msg.data[3]
+
+
+	def OnUpdateMotor(self, msg): #debug
+		self.motorL_angv = msg.data[0]
+		self.motorR_angv = msg.data[1]
+
+	def Disp(self, event):
+		print('')
+		print('pnd ang  : ', self.pendulum_ang)
+		print('pnd angv : ', self.pendulum_angv)
+		print('car pos  : ', self.car_pos)
+		print('car vel  : ', self.car_vel)
+		print('')
+		print('    motorL_angv : 'self.motorL_angv)
+		print('    motorR_angv : 'self.motorR_angv)
+
+
+if __name__=="__main__":
+	node = CheckNode():
+
---
+++ scripts/state_talker.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+
+import gpiozero
+import rospy
+from std_msgs.msg import Float32MultiArray, Bool
+
+ENCODER_VALUE_2_RAD = 3.14159 * 2.0
+MOTOR_ENCODER_STEP_2_RAD = 3.14159 / 40.0
+MOTOR_ENCODER_MAX_STEP = 40
+TIRE_RADIUS = 0.05
+FAIL_ANG_RAD = 3.14159 / 3.0
+			
+
+def check_fail(ang):
+	return False
+#	if ang > FAIL_ANG_RAD or ang < -FAIL_ANG_RAD:
+#		return True
+#	else:
+#		return False
+
+
+def main():
+	pendulum_encoder = gpiozero.MCP3208(channel=0)
+	encoder_a = rospy.get_param('/state_talker/motorL_encoder_a')
+	encoder_b = rospy.get_param('/state_talker/motorL_encoder_b')
+	motorL_encoder = gpiozero.RotaryEncoder(a=encoder_a, b=encoder_b, max_steps=MOTOR_ENCODER_MAX_STEP, wrap=True)
+	encoder_a = rospy.get_param('/state_talker/motorR_encoder_a')
+	encoder_b = rospy.get_param('/state_talker/motorR_encoder_b')
+	motorR_encoder = gpiozero.RotaryEncoder(a=encoder_a, b=encoder_b, max_steps=MOTOR_ENCODER_MAX_STEP, wrap=True)
+	
+	l = 5
+	pendulum_ang_before = 0.0
+	pendulum_latest_delta_angs = [0.0] * 5
+	motorR_ang_before = 0.0
+	motorR_latest_delta_angs = [0.0] * 5
+	motorL_ang_before = 0.0
+	motorL_latest_delta_angs = [0.0] * 5
+	s = 1.0 - (0.5 ** l)
+
+	try:
+		rospy.init_node('state_talker', anonymous=True)
+		#state=[car's pos  ,  car's vel  ,  pendulum's ang  ,  pendulum's angv]
+		pub_state = rospy.Publisher('state', Float32MultiArray, queue_size=10)
+		
+		pub_motor = rospy.Publisher('motor', Float32MultiArray, queue_size=10)
+		pub_fail = rospy.Publisher('failed', Bool, queue_size=10)
+		r = rospy.Rate(50)
+		delta_t = 1.0 / 50.0
+
+		while not rospy.is_shutdown():
+			pendulum_ang = pendulum_encoder.value * ENCODER_VALUE_2_RAD - 3.14159
+			if check_fail(pendulum_ang):
+#				print('check fail')
+				pub_fail.publish(True)
+#				break
+			else:
+				for i in range(1, l):
+					pendulum_latest_delta_angs[i] = pendulum_latest_delta_angs[i - 1]
+					motorR_latest_delta_angs[i] = motorR_latest_delta_angs[i - 1]
+					motorL_latest_delta_angs[i] = motorL_latest_delta_angs[i - 1]
+				motorL_ang = float(motorL_encoder.steps) * MOtOR_ENCODER_STEP_2_RAD
+				motorR_ang = float(motorR_encoder.steps) * MOtOR_ENCODER_STEP_2_RAD
+
+				pendulum_latest_delta_angs[0] = pendulum_ang - pendulum_ang_before
+				motorL_latest_delta_angs[0] = motorL_ang - motorL_ang_before
+				if motorL_latest_delta_angs[0] > 3.14159:
+					motorL_latest_delta_angs[0] -= 2.0 * 3.14159
+				if motorL_latest_delta_angs[0] < -3.14159:
+					motorL_latest_delta_angs[0] += 2.0 * 3.14159
+				motorR_latest_delta_angs[0] = motorR_ang - motorR_ang_before
+				if motorR_latest_delta_angs[0] > 3.14159:
+					motorR_latest_delta_angs[0] -= 2.0 * 3.14159
+				if motorR_latest_delta_angs[0] < -3.14159:
+					motorR_latest_delta_angs[0] += 2.0 * 3.14159
+
+				car_pos = (motorL_ang + motorR_ang) * TIRE_RADIUS / 2.0
+				
+				pendulum_angv = 0.0
+				motorL_angv = 0.0
+				motorR_angv = 0.0
+				d_p = 0.0
+				d_R = 0.0
+				d_L = 0.0
+				p = 1.0
+				for i in range(l):
+					p *= 0.5
+					d_p += pendulum_delta_angs[i]
+					d_R += motorR_delta_angs[i]
+					d_L += motorL_delta_angs[i]
+					pendulum_angv += p * d_p / (float(i + 1) * delta_t)
+					motorL_angv += p * d_L / (float(i + 1) * delta_t)
+					motorR_angv += p * d_R / (float(i + 1) * delta_t)
+
+				car_vel = (motorL_angv + motorR_angv) * TIRE_RADIUS / 2.0
+
+				arr = [pendulum_ang, pendulum_angv, car_pos, car_vel]
+				pub_state.publish(Float32MultiArray(data=arr))
+				
+				pub_motor.publish(Float32MultiArray(data=[motorL_angv, motorR_angv]))
+				
+				pendulum_ang_before = pendulum_ang
+				motorL_ang_before = motorL_ang
+				motorR_ang_before = motorR_ang
+			r.sleep()
+
+	except rospy.ROSInterruptException: pass
+
+
+if __name__ == '__main__':
+	main()
