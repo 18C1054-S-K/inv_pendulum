3afecfb399b3de4475f5555d66ecbc2d04b70bb7
18C1054_S_K <s18c1054uc@s.chibakoudai.jp>
2022-07-08 15:59:31 +0900

    debug motor_check

--- scripts/motor_check.py
+++ scripts/motor_check.py
@@ -6,12 +6,13 @@ import gpiozero
 
 if __name__ == "__main__":
 	rospy.init_node('motor_check', anonymous=True)
-	motorL = gpiozero.Motor(forward=27, backward=22)
-	motorR = gpiozero.Motor(forward=23, backward=24)
+	motorL = gpiozero.Motor(forward=22, backward=27)
+	motorR = gpiozero.Motor(forward=24, backward=23)
 
+	time.sleep(2.0)
 	v = rospy.get_param('/motor_check/volt')
 	o = v / 9.0
 	motorL.forward(o)
 	motorR.forward(o)
-	print('output : ', o, '%')
+	print('output : ', o*100.0, '%')
 
