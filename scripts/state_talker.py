be76b2d63dc2340b70d54599edfb42cf2d7a5e7a
18C1054_S_K <s18c1054uc@s.chibakoudai.jp>
2022-07-08 12:01:07 +0900

    fix state_talker

--- scripts/state_talker.py
+++ scripts/state_talker.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 #import spidev
+import time
 import gpiozero
 import rospy
 from std_msgs.msg import Float32MultiArray, Bool
@@ -8,7 +9,7 @@ from std_msgs.msg import Float32MultiArray, Bool
 ENCODER_VALUE_2_RAD = 3.14159 * 2.0
 MOTOR_ENCODER_STEP_2_RAD = 3.14159 / 40.0
 MOTOR_ENCODER_MAX_STEP = 40
-TIRE_RADIUS = 0.05
+TIRE_RADIUS = 0.025
 FAIL_ANG_RAD = 3.14159 / 3.0
 			
 
@@ -39,6 +40,8 @@ def main():
 	motorR_latest_delta_angs = [0.0] * 3
 	motorL_ang_before = 0.0
 	motorL_latest_delta_angs = [0.0] * 3
+	time_before = time.perf_counter
+	latest_delta_times = [1.0/100.0] * 3
 	s = 1.0 - (0.5 ** l)
 
 	try:
@@ -50,6 +53,7 @@ def main():
 		pub_fail = rospy.Publisher('failed', Bool, queue_size=10)
 		r = rospy.Rate(100)
 		delta_t = 1.0 / 100.0
+		
 
 		while not rospy.is_shutdown():
 			pendulum_ang = pendulum_encoder.value * ENCODER_VALUE_2_RAD - 3.14159
@@ -62,8 +66,10 @@ def main():
 					pendulum_latest_delta_angs[i] = pendulum_latest_delta_angs[i - 1]
 					motorR_latest_delta_angs[i] = motorR_latest_delta_angs[i - 1]
 					motorL_latest_delta_angs[i] = motorL_latest_delta_angs[i - 1]
+					latest_delta_times[i] = latest_delta_times[i - 1]
 				motorL_ang = float(motorL_encoder.steps) * MOTOR_ENCODER_STEP_2_RAD
 				motorR_ang = float(motorR_encoder.steps) * MOTOR_ENCODER_STEP_2_RAD
+				time_now = time.perf_counter
 
 				pendulum_latest_delta_angs[0] = pendulum_ang - pendulum_ang_before
 				motorL_latest_delta_angs[0] = motorL_ang - motorL_ang_before
@@ -76,6 +82,7 @@ def main():
 					motorR_latest_delta_angs[0] -= 2.0 * 3.14159
 				if motorR_latest_delta_angs[0] < -3.14159:
 					motorR_latest_delta_angs[0] += 2.0 * 3.14159
+				latest_delta_times[0] = time_now - time_before
 
 				car_pos = (motorL_ang + motorR_ang) * TIRE_RADIUS / 2.0
 				
@@ -85,15 +92,17 @@ def main():
 				d_p = 0.0
 				d_R = 0.0
 				d_L = 0.0
+				d_t = 0.0
 				p = 1.0
 				for i in range(l):
 					p *= 0.5
 					d_p += pendulum_latest_delta_angs[i]
 					d_R += motorR_latest_delta_angs[i]
 					d_L += motorL_latest_delta_angs[i]
-					pendulum_angv += p * d_p / (float(i + 1) * delta_t)
-					motorL_angv += p * d_L / (float(i + 1) * delta_t)
-					motorR_angv += p * d_R / (float(i + 1) * delta_t)
+					d_t += latest_delta_times[i]
+					pendulum_angv += p * d_p / (float(i + 1) * d_t)
+					motorL_angv += p * d_L / (float(i + 1) * d_t)
+					motorR_angv += p * d_R / (float(i + 1) * d_t)
 
 				car_vel = (motorL_angv + motorR_angv) * TIRE_RADIUS / 2.0
 
@@ -105,6 +114,7 @@ def main():
 				pendulum_ang_before = pendulum_ang
 				motorL_ang_before = motorL_ang
 				motorR_ang_before = motorR_ang
+				time_before = time_now
 			r.sleep()
 
 	except rospy.ROSInterruptException: pass
