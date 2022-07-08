8f33e9c23fe50c29ffb79fb09e0848495a02bb06
18C1054_S_K <s18c1054uc@s.chibakoudai.jp>
2022-07-08 15:30:27 +0900

    add motor_check

---
+++ scripts/motor_check.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import time
+import rospy
+import gpiozero
+
+if __name__ == "__main__":
+	rospy.init_node('motor_check', anonymous=True)
+	motorL = gpiozero.Motor(forward=27, backward=22)
+	motorR = gpiozero.Motor(forward=23, backward=24)
+
+	v = rospy.get_param('/motor_check/volt')
+	o = v / 9.0
+	motorL.rotate(o)
+	motorR.rotate(o)
+
